cb914b814e0ce1d9cb0a4617923d9e61cc2dfd93
pabgom <pablojesus.g.p@gmail.com>
2021-04-25 12:48:51 +0100

    Add Seed Database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,14 @@
 import { config } from 'dotenv';
+import { Roles } from './roles';
 
 config();
 
 const configurations = {
     PORT: process.env.PORT || 3000,
+    INITIAL_USER_NAME: process.env.INITIAL_USER_NAME || 'admin@admin.com',
+    INITIAL_USER_PASSWORD: process.env.INITIAL_USER_PASSWORD || 'dummy',
     JWT_SECRETKEY: process.env.JWT_SECRETKEY || '',
+    DB_SYNCRONIZE: process.env.DB_SYNCRONIZE || 'false',
     DB_USER: process.env.DB_USER || 'postgres',
     DB_PASSWORD: process.env.DB_PASSWORD || '',
     DB_HOST: process.env.DB_HOST || 'localhost',
@@ -13,3 +17,5 @@ const configurations = {
 };
 
 export default configurations;
+
+export { Roles };
---
+++ src/config/roles.ts
@@ -0,0 +1,4 @@
+export enum Roles {
+    Admin = 1,
+    Basic = 2
+}
