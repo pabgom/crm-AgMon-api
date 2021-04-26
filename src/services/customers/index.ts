cf32f745e8eb98b69fbe3f85df98cb8a461bb625
pabgom <pablojesus.g.p@gmail.com>
2021-04-26 16:52:39 +0100

    Add relation from Customer to USer when create

--- src/services/customers/index.ts
+++ src/services/customers/index.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from './../../entity/user.entity';
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { CustomerEntity } from './../../entity';
@@ -13,7 +14,11 @@ export const getCustomer = async (req: Request, res: Response): Promise<Response
 };
 
 export const createCustomer = async (req: Request, res: Response): Promise<Response> => {
-    const newCustomer = getRepository(CustomerEntity).create(req.body);
+    const newCustomer = getRepository(CustomerEntity).create(req.body as CustomerEntity);
+
+    const user = await getRepository(UserEntity).findOne(req.body.id);
+    newCustomer.createdBy = user;
+
     const results = await getRepository(CustomerEntity).save(newCustomer);
     return res.status(200).json(results);
 };
@@ -22,6 +27,10 @@ export const updateCustomer = async (req: Request, res: Response): Promise<Respo
     const customer = await getRepository(CustomerEntity).findOne(req.params.id);
     if (customer) {
         getRepository(CustomerEntity).merge(customer, req.body);
+
+        const user = await getRepository(UserEntity).findOne(req.body.id);
+        customer.modifiedBy = user;
+
         const result = await getRepository(CustomerEntity).save(customer);
         return res.status(200).json(result);
     }
