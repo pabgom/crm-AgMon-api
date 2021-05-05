785a225c332cf97dbdead0b769d9d6f06d4a6de7
pabgom <pablojesus.g.p@gmail.com>
2021-05-05 10:03:28 +0100

    Disable Swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
-import YAML from 'yamljs';
-import swaggerUI from 'swagger-ui-express';
 import morganMiddleware from './middleware/morganMiddleware';
 import config from './config';
 import routes from './routes';
@@ -32,8 +30,7 @@ InitializeDB()
         app.use(bodyParser.json());
 
         // Load Swagger documentation
-        const swaggerDocument = YAML.load(path.join(__dirname, 'config/swagger.yml'));
-        app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+        // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
         app.use(routes);
         app.use(apiErrorHandler);
